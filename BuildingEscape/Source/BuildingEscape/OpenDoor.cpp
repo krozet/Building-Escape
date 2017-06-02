ccfa6b34aa7841906df2d58125bbac7e929966e0
Keawa Rozet <keawa.rozet@yahoo.com>
2017-06-02 15:33:03 -0700

    BE25 Lighting adjustment

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -49,7 +49,6 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate()
 
 	for (const auto* Actor : OverlappingActors) {
 		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
-		UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate"), *Actor->GetName())
 	}
 	return TotalMass;
 }
